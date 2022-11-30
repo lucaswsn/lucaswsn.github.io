105222a216009f8811153eb79c1909f539e16a83
lucaswsn <106093876+lucaswsn@users.noreply.github.com>
2022-11-30 11:37:33 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,12 +19,12 @@ window.onscroll = () => {
 
 const sr = ScrollReveal ({
 	distance: '40px',
-	duration: 2700,
+	duration: 2500,
 	reset: true
 })
 
-sr.reveal('.home-text',{delay:350, origin:'left'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
+sr.reveal('.home-text',{delay:300, origin:'left'})
+sr.reveal('.home-img',{delay:300, origin:'right'})
 
 sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
 
